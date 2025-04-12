27e354396d969ea9a3c38795bdb1e3c0a23f9935
Aoao <931203821@qq.com>
2025-04-12 19:26:35 +0800

    feat:完成了店铺营业状态的功能

--- sky-server/src/main/java/com/sky/controller/user/ShopController.java
+++ sky-server/src/main/java/com/sky/controller/user/ShopController.java
@@ -1,4 +1,4 @@
-package com.sky.controller.admin;
+package com.sky.controller.user;
 
 import com.sky.result.Result;
 import io.swagger.annotations.Api;
@@ -6,30 +6,22 @@ import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author aoao
- * @create 2025-04-12-11:30
+ * @create 2025-04-12-13:09
  */
-@RestController("shopShopController")
-@RequestMapping("/admin/shop")
+@RestController("userShopController")
+@RequestMapping("/user/shop")
 @Slf4j
 @Api(tags = "店铺相关接口")
 public class ShopController {
 
     @Autowired
     private RedisTemplate redisTemplate;
-
-    @PutMapping("/{status}")
-    @ApiOperation("设置营业状态")
-    public Result setStatus(@PathVariable("status") Integer status) {
-        log.info("营业状态为:{}", status == 1 ? "营业" : "打样");
-        redisTemplate.opsForValue().set("SHOP_STATUS", status);
-        return Result.success();
-    }
 
     @GetMapping("/status")
     @ApiOperation("查询营业状态")
@@ -38,5 +30,4 @@ public class ShopController {
         log.info("获取到营业状态为:{}", status);
         return Result.success(status);
     }
-
 }
